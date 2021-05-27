c513a12f31dd77679ca92a4a0345399922c3aacc
GavinCoupDigipen <53310510+GavinCoupDigipen@users.noreply.github.com>
2021-05-26 17:46:35 -0700

    Added Parralaxing
    Added Parralaxing to the gameplay scene, it follows the player.

---
+++ Assets/Programming/Parralax.cs
@@ -0,0 +1,31 @@
+﻿//------------------------------------------------------------------------------
+//
+// File Name:	Parralax.cs
+// Author(s):	Gavin Cooper (gavin.cooper)
+// Project:		Asteroids
+// Course:		WANIC VGP
+//
+// Copyright © 2021 DigiPen (USA) Corporation.
+//
+//------------------------------------------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parralax : MonoBehaviour
+{
+    [Tooltip("What the parralax is tracking")]
+    public GameObject Target;
+    [Tooltip("How far target has to move to move object 1 unit")]
+    public float Scale;
+
+    private float LerpSpeed = 0.025f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        var NewPosition = new Vector2(-Target.transform.position.x / Scale, -Target.transform.position.x / Scale);
+        transform.position = Vector2.Lerp(transform.position, NewPosition, LerpSpeed);
+    }
+}
---
+++ Assets/Programming/Un-Used/ScrollingPerralax.cs
@@ -0,0 +1,47 @@
+﻿//------------------------------------------------------------------------------
+//
+// File Name:	ScrollingPerralax.cs
+// Author(s):	Gavin Cooper (gavin.cooper)
+// Project:		Asteroids
+// Course:		WANIC VGP
+//
+// Copyright © 2021 DigiPen (USA) Corporation.
+//
+//------------------------------------------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingPerralax : MonoBehaviour
+{
+    public float WarpPoint;
+    public float Speed;
+
+    private Vector2 StartingLocation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+        StartingLocation = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Negative speed, right to left
+        if (transform.position.x < WarpPoint && Speed < 0)
+        {
+            transform.position = StartingLocation;
+        }
+        // Negative speed, right to left
+        else if (transform.position.x > WarpPoint && Speed > 0)
+        {
+            transform.position = StartingLocation;
+        }
+
+        // Move object
+        transform.position = new Vector2(transform.position.x + (Speed * Time.deltaTime), transform.position.y);
+    }
+}
